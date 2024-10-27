fbb1771525e1b4f91f637acd375ba3e3ba7965cb
hypnos <hypnos@debian-BULLSEYE-live-builder-AMD64>
2024-10-27 22:08:14 +0400

    countries and cities

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -10,9 +10,9 @@ use Illuminate\Support\Str;
 
 class CompanyService
 {
-    public function __construct(private readonly CompanyRepository $companyRepository,
-                                private readonly CompanyUserRepository $companyUserRepository,
-                                private readonly AddressService $addressService,
+    public function __construct(private readonly CompanyRepository         $companyRepository,
+                                private readonly CompanyUserRepository     $companyUserRepository,
+                                private readonly AddressService            $addressService,
                                 private readonly CompanyInformationService $companyInformationService)
     {
     }
@@ -21,21 +21,23 @@ class CompanyService
     {
         return $this->companyUserRepository->findByUser($user->id);
     }
-    public function create($user,$identification_number, $company_type_id, $company_information, $company_address)
+
+    public function create($user, $identification_number, $company_type_id, $company_information, $company_address)
     {
-        \DB::transaction(function ()use($user,$identification_number, $company_type_id, $company_information, $company_address) {
-        $company = $this->companyRepository->create($identification_number, $company_type_id);
-        $this->companyInformationService->create($company->id, $company_information);
-        foreach($company_address as $companyAddress) {
-            $this->addressService->createOrUpdate($company->id, $companyAddress['address'], $companyAddress['city'], $companyAddress['state'], $companyAddress['lat'],
-                $companyAddress['long'], $companyAddress['email'],
-                $companyAddress['phone'],
-                $companyAddress['postal_code'], $companyAddress['is_same_time'], $companyAddress['start_time'], $companyAddress['end_time'], null);
+        \DB::transaction(function () use ($user, $identification_number, $company_type_id, $company_information, $company_address) {
+            $company = $this->companyRepository->create($identification_number, $company_type_id);
+            $this->companyInformationService->create($company->id, $company_information);
+            foreach ($company_address as $companyAddress) {
+                $this->addressService->createOrUpdate($company->id, $companyAddress['address'], $companyAddress['city'], $companyAddress['state'], $companyAddress['lat'],
+                    $companyAddress['long'], $companyAddress['email'],
+                    $companyAddress['phone'],
+                    $companyAddress['postal_code'], $companyAddress['is_same_time'], $companyAddress['start_time'], $companyAddress['end_time'], null);
+
+            }
             $this->companyUserRepository->create($company->id, $user->id);
-        }
             return $company;
         });
-       return 0;
+        return 0;
     }
 
     public function update($company_id, $companyInformation)
