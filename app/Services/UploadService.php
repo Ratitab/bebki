0b2e77f20f40273677b66236666c9981f890b60f
hypnos <hypnos@debian-BULLSEYE-live-builder-AMD64>
2024-11-01 04:07:32 +0400

    images

--- app/Services/UploadService.php
+++ app/Services/UploadService.php
@@ -19,13 +19,20 @@ class UploadService
 
     public function uploadProductImages($images,$user,$image_for)
     {
+        $imageUrls = [];
         if (is_array($images)) {
             foreach ($images as $image) {
                 try {
                     // Upload image to DigitalOcean Spaces
-                    $imagePath = Storage::disk('spaces')->put($user->id .'/'.$image_for.'/product-images', $image, 'public');
-                    $imageUrls[] = Storage::disk('spaces')->url($imagePath);
-                } catch (\Exception $e) {
+                    $imagePath = Storage::disk('spaces')->put($user->id . '/' . $image_for . '/product-images', $image, 'public');
+                    $originalUrl = Storage::disk('spaces')->url($imagePath);
+
+                    // Replace the Spaces URL with the CDN URL
+                    $cdnUrl = 'https://gegold.fra1.cdn.digitaloceanspaces.com';
+                    $cdnImageUrl = str_replace('https://gegold.fra1.digitaloceanspaces.com', $cdnUrl, $originalUrl);
+
+                    $imageUrls[] = $cdnImageUrl;
+                }catch (\Exception $e) {
                     // Log the error or handle it accordingly
                     \Log::error('Image upload failed: ' . $e->getMessage());
                 }
