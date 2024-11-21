b3279548595ad0028c58cc66f7c9b61ba8637f23
hypnos <hypnos@debian-BULLSEYE-live-builder-AMD64>
2024-11-21 23:37:41 +0400

    products

---
+++ app/Tasks/CheckPaidAdvExpiration.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tasks;
+
+use App\Models\Products\Product;
+use Carbon\Carbon;
+
+class CheckPaidAdvExpiration
+{
+    public function __invoke()
+    {
+        // Your model import here
+        Product::where('paid_adv_expires_at', '<', Carbon::now()->toDateTime()->format('Y-m-d\TH:i:s.v\Z'))
+            ->update([
+                '$unset' => [
+                    'paid_adv_expires_at' => 1,
+                    'is_paid_adv' => 1
+                ]
+            ]);
+    }
+}
