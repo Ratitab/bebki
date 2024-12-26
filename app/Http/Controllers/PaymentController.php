546c7294ac61e2960ba42a432f0d28812e420dd9
hypnos <hypnos@debian-BULLSEYE-live-builder-AMD64>
2024-12-26 19:51:55 +0400

    stripe

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -74,7 +74,7 @@ class PaymentController extends Controller
     public function handleWebhook(Request $request)
     {
 
-        $payload = $request->getContent();
+        $payload = json_decode($request->getContent());
         if($payload['type'] === 'charge.succeeded' && $payload['data']['object']['paid'] === true){
             $payment = $this->stripeService->updatePaymentStatus($payload['data']['object']['metadata']['order_id'],'PAID');
             if($payment){
