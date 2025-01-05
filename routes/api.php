cfb0535296635cba04698cab0d4fac3efb0dc939
hhypnos <giorgi.gogitidze164@gmail.com>
2025-01-05 22:19:36 +0400

    turnstile

--- routes/api.php
+++ routes/api.php
@@ -60,11 +60,11 @@ Route::post('/stripe/webhook', [PaymentController::class, 'handleWebhook']);
 |
 |
 */
-Route::post('/signup', [AuthenticationController::class, 'registration']);
-Route::post('/signin', [AuthenticationController::class, 'login']);
+Route::post('/signup', [AuthenticationController::class, 'registration'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
+Route::post('/signin', [AuthenticationController::class, 'login'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
 Route::post('/otp-registration', [AuthenticationController::class, 'otp_registration']);
 Route::post('/otp-forgot-password', [AuthenticationController::class, 'otp_forgot_password']);
-Route::post('/forgot-password', [AuthenticationController::class, 'forgot_password']);
+Route::post('/forgot-password', [AuthenticationController::class, 'forgot_password'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -78,14 +78,14 @@ Route::middleware(['auth:api'])->group(function () {
         $user = auth()->user();
         return $user->append('information');
     });
-    Route::post('/update-email-or-phone', [AuthenticationController::class, 'update_email_or_phone']);
+    Route::post('/update-email-or-phone', [AuthenticationController::class, 'update_email_or_phone'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
     Route::post('/upload-profile-image', [AuthenticationController::class, 'upload_images']);
     Route::post('/otp-update-email-or-phone', [AuthenticationController::class, 'otp_update_email_or_phone']);
 
-    Route::put('/update-user-information', [AuthenticationController::class, 'update_user_information']);
+    Route::put('/update-user-information', [AuthenticationController::class, 'update_user_information'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
 
 
-    Route::post('/change-password', [AuthenticationController::class, 'change_password']);
+    Route::post('/change-password', [AuthenticationController::class, 'change_password'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
 
     Route::get('/user/limits', [LimitInformationController::class, 'user_limits']);
     Route::get('/user/count-favourites', [ProductController::class, 'count_user_favourites']);
@@ -104,18 +104,18 @@ Route::middleware(['auth:api'])->group(function () {
     Route::prefix('company')->group(function () {
         Route::get('/', [CompanyController::class, 'index']);
         Route::get('/{company_id}', [CompanyController::class, 'show']);
-        Route::post('/add', [CompanyController::class, 'store']);
+        Route::post('/add', [CompanyController::class, 'store'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
         Route::post('/upload-profile-image', [CompanyController::class, 'upload_images']);
-        Route::put('/update/{company_id}', [CompanyController::class, 'update']);
-        Route::delete('/delete/{company_id}', [CompanyController::class, 'delete']);
+        Route::put('/update/{company_id}', [CompanyController::class, 'update'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
+        Route::delete('/delete/{company_id}', [CompanyController::class, 'delete'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
 
         Route::get('/limits/{company_id}', [LimitInformationController::class, 'company_limits']);
     });
 
 
     Route::prefix('product')->group(function () {
-        Route::post('/add', [ProductController::class, 'store']);
-        Route::put('/update/{product_id}', [ProductController::class, 'update']);
+        Route::post('/add', [ProductController::class, 'store'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
+        Route::put('/update/{product_id}', [ProductController::class, 'update'])->middleware(\NjoguAmos\Turnstile\Turnstile::class);
         Route::put('/update-product-order/{product_id}', [ProductController::class, 'update_product_order']);
         Route::post('/upload-images', [ProductController::class, 'upload_images']);
         Route::post('/make-favourite', [ProductController::class, 'make_favourite']);
